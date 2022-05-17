21233261ab4209f632bfd3e807bd20ff0d6b842a
jmemcc <66285033+jmemcc@users.noreply.github.com>
2022-05-17 17:46:19 +0800

    Began rake-c; currently parses file only

--- Project/rake-c/client.c
+++ Project/rake-c/client.c
@@ -1,87 +1,240 @@
-// Client program in C99
-// Compiled with:
-//	mycc='cc -std=c99 -Wall -Werror'
-#include "client.h"
+// cc -std=c99 -Wall -Werror client.c -o client
+// ./client
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <errno.h>
 
-// bool prefix(const char *pre, const char *str)
-// {
-//     return strncmp(pre, str, strlen(pre)) == 0;
-// }
+// -----------------------------------------------------------------------------
 
-int default_rakefile()
+#define MAX_COMMANDS 256        // max number of commands in actionset
+#define MAX_SERVERS 256         // max number of host servers
+#define MAX_ACTIONSETS 256      // max number of actionsets in rakefile
+#define MAX_NAME_LEN 64         // max length of a name
+
+// -----------------------------------------------------------------------------
+
+typedef struct COMMAND
 {
-    char path[1024];
-    if (getcwd(path, sizeof(path)) == NULL)
+    char location[MAX_NAME_LEN];    // remote/local
+    char command[MAX_NAME_LEN];     // command to execute
+    char requires[MAX_COMMANDS][MAX_NAME_LEN];    // files needed for execution
+} COMMAND;
+
+typedef struct ACTIONSET
+{
+    int index;                      // actionset number
+    int num_actions;                // number of actions in actionset
+    COMMAND commands[MAX_COMMANDS]; // array of commands
+} ACTIONSET;
+
+typedef struct SERVER
+{
+    char host[MAX_NAME_LEN];   
+    int port;             
+
+} SERVER;
+
+// -----------------------------------------------------------------------------
+
+// Global actionset variable for client
+ACTIONSET actionsets[MAX_COMMANDS];
+SERVER servers[MAX_SERVERS];
+
+// -----------------------------------------------------------------------------
+
+// Read in the rakefile and store the actionsets in the actionset array
+int read_rakefile(FILE *rake_fp)
+{
+    char linebuf[256];
+    int actionset_index = -1;
+    int server_index    = -1;
+    int command_index   = -1;
+    int requires_index  = -1;
+    int default_port    = -1;
+
+    printf("[r.c] reading rakefile...\n");
+
+    // read file line by line
+    while (fgets(linebuf, 512, rake_fp) != NULL)
     {
-        perror("getcwd() error");
-        return 1;
-    }
-    char *filename = "/Rakefile";
-    char *ptr = strrchr(path, '/');
-    *ptr = '\0';
-    strcat(path, filename);
-    printf("Opening Rakefile at: %s\n", path);
+        linebuf[strlen(linebuf) - 1] = '\0';
 
-    FILE *fp = fopen(path, "r");
-
-    if (fp == NULL)
-    {
-        printf("Error: could not open file %s", filename);
-        return 1;
-    }
-
-    const unsigned MAX_LENGTH = 256;
-    char buffer[MAX_LENGTH];
-
-    int default_port; // default port number
-    int actionset_num = 1;
-    char *hosts;       // 22 is the maximum length of a hostname (192.168.0.0:65535)
-    char *commands; // array of commands
-    char *actionsets [MAX_LENGTH][MAX_LENGTH]; // array of actionsets
-
-    while (fgets(buffer, MAX_LENGTH, fp) != NULL)
-    {
-        // if the line starts with a '#' character, ignore it
-        if (buffer[0] == '#')
-        {
+        // ignore line if comment, newline, or empty
+        if (linebuf[0] == '#' || linebuf[0] == '\n' || linebuf[0] == '\r')
             continue;
-        }
-        // else if it starts with a '\n' character, ignore it
-        else if (buffer[0] == '\n')
+        // finds default port number
+        else if (strncmp(linebuf, "PORT", strlen("PORT")) == 0)
         {
-            continue;
+            default_port = atoi(linebuf + strlen("PORT = "));
+            // printf("> parse: default port set to %d\n", default_port);
         }
-        // else if it starts with a tab character, print it
-        else if (buffer[0] == 'a')
+        // finds host servers and stores in server array
+        else if (strncmp(linebuf, "HOST", strlen("HOST")) == 0)
         {
-            printf("%s", buffer);
+            char *token = strtok(linebuf + strlen("HOST = "), " ");
+            while (token != NULL)
+            {
+                server_index++;
+                strcpy(servers[server_index].host, token);
+                token = strtok(NULL, " ");
+                if (strchr(servers[server_index].host, ':') == NULL)
+                {
+                    sprintf(servers[server_index].host, "%s:%d", servers[server_index].host, default_port);
+                }
+                // printf("> parse: found host '%s'\n", servers[server_index].host);
+            }
         }
+        else if (strncmp(linebuf, "actionset", strlen("actionset")) == 0)
+        {
+            actionset_index++;
+            command_index = -1;
+            requires_index = -1;
+        }
+        // finds requirements of lines
+        else if (linebuf[0] == '\t' && linebuf[1] == '\t')
+        {
+            // split line on space and store in array
+            char *token = strtok(linebuf + 2 + strlen("requires"), " ");
 
-        // else if it starts with a '\r' character, ignore it
-        else if (buffer[0] == '\r')
+            while (token != NULL)
+            {
+                requires_index++;
+                strcpy(actionsets[actionset_index].commands[command_index].requires[requires_index], token);
+                // printf("%s ", actionsets[actionset_index].commands[command_index].requires[requires_index]);
+                token = strtok(NULL, " ");
+            }
+        }
+        // finds commands in rakefile
+        else if (linebuf[0] == '\t')
         {
-            continue;
+            command_index++;
+            actionsets[actionset_index].num_actions++;
+
+            // determines whether line is remote or locally executed
+            if (strncmp(linebuf, "\tremote-", 8) == 0)
+            {
+                strcpy(actionsets[actionset_index].commands[command_index].location, "remote");
+                strcpy(actionsets[actionset_index].commands[command_index].command, linebuf + 8);
+                // printf("(%i) remote: %s\n", command_index, actionsets[actionset_index].commands[command_index].command);
+            }
+            else
+            {
+                strcpy(actionsets[actionset_index].commands[command_index].location, "local");
+                strcpy(actionsets[actionset_index].commands[command_index].command, linebuf + 1);
+                // printf("local: %s\n", actionsets[actionset_index].commands[command_index].command);
+            }
         }
     }
-
-
-    
-    // while (fgets(buffer, MAX_LENGTH, fp))
-    //     printf("%s", buffer);
-
-    // close the file
-    fclose(fp);
-
-    return 0;
+    printf("[r.c] done reading rakefile\n");
+    return EXIT_SUCCESS;
 }
 
-int main() 
+// -----------------------------------------------------------------------------
+
+int get_num_actionsets()
 {
-    default_rakefile();
-    return 0;
+    int num_actionsets = 0;
+    for (int i = 0; i < MAX_COMMANDS; i++)
+    {
+        if (actionsets[i].num_actions > 0)
+        {
+            num_actionsets++;
+        }
+    }
+    return num_actionsets;
+}
+
+int get_num_servers()
+{
+    int num_servers = 0;
+    for (int i = 0; i < MAX_SERVERS; i++)
+    {
+        if (strcmp(servers[i].host, "") != 0)
+        {
+            num_servers++;
+        }
+    }
+    return num_servers;
+}
+
+void print_actionsets()
+{
+    printf("[r.c] printing actionsets ");
+    printf("(%i found)\n", get_num_actionsets());
+    for (int i = 0; i < MAX_COMMANDS; i++)
+    {
+        if (actionsets[i].num_actions > 0)
+        {
+            printf("\tactionset%d\n", i+1);
+            for (int j = 0; j < actionsets[i].num_actions; j++)
+            {
+                printf("\t\t%s %s\n", actionsets[i].commands[j].location, actionsets[i].commands[j].command);
+                for (int k = 0; k < MAX_COMMANDS; k++)
+                {
+                    if (strcmp(actionsets[i].commands[j].requires[k], "") != 0)
+                    {
+                        printf("\t\t\trequires:'%s' \n", actionsets[i].commands[j].requires[k]);
+                    }
+                }
+            }
+        }
+    }
+}
+
+void print_servers()
+{
+    printf("[r.c] printing servers ");
+    printf("(%d found)\n", get_num_servers());
+    for (int i = 0; i < MAX_SERVERS; i++)
+    {
+        if (strcmp(servers[i].host, "") != 0)
+        {
+            printf("\t(%d) %s\n", i + 1, servers[i].host);
+        }
+    }
+}
+
+// -----------------------------------------------------------------------------
+
+int main(int argc, char *argv[])
+{
+    FILE *rake_fp;
+
+    if (argc > 2)
+    {
+        printf("[r.c] using rakefile path specified\n");
+        rake_fp = fopen(argv[1], "r");
+    } 
+    else
+    {
+        printf("[r.c] using default rakefile path\n");
+        char cwd[256];
+        if (getcwd(cwd, 256) == NULL)
+            return EXIT_FAILURE;
+        // remove the last '/' from the cwd
+        cwd[strlen(cwd) - strlen("rake-c")] = '\0';
+        strcat(cwd, "Rakefile");
+        printf("\t%s\n", cwd);
+        rake_fp = fopen(cwd, "r");
+    }
+
+    if (rake_fp == NULL)
+    {
+        perror("fopen");
+        return EXIT_FAILURE;
+    }
+
+    if (read_rakefile(rake_fp) != EXIT_SUCCESS)
+        return EXIT_FAILURE;
+    
+    print_actionsets();
+    print_servers();
+
+    return EXIT_SUCCESS;
 }
